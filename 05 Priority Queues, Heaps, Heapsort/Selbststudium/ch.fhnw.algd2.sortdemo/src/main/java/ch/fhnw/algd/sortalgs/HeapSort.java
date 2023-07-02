6a028422b0056e0a10dd060e67362335d573b4e1
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2023-07-02 17:33:32 +0200

    solution heapsort

--- 05 Priority Queues, Heaps, Heapsort/Selbststudium/ch.fhnw.algd2.sortdemo/src/main/java/ch/fhnw/algd/sortalgs/HeapSort.java
+++ 05 Priority Queues, Heaps, Heapsort/Selbststudium/ch.fhnw.algd2.sortdemo/src/main/java/ch/fhnw/algd/sortalgs/HeapSort.java
@@ -10,5 +10,41 @@ public class HeapSort implements SortAlg {
 	@Override
 	public void run(SortData data) {
 		// TODO implement HeapSort here
+
+		creatFloydMaxHeap(data);
+		heapSort(data);
+
+	}
+
+	private void creatFloydMaxHeap (SortData data) {
+		int size = data.size();
+		for (int i = size -1; i > 0; i--) {
+
+		}
+	}
+
+	private void heapSort(SortData data) {
+		int size = data.size();
+		for (int i = size - 1; i > 0; i--) {
+			data.swap(0, i);
+			siftDown(data, 0, i);
+		}
+	}
+
+	private void siftDown(SortData data, int parent, int size) {
+		int child = indexOfLargerChild(data, parent, size);
+		while (child < size && data.less(parent, child)) {
+			data.swap(parent, child);
+			parent = child;
+			child = indexOfLargerChild(data, child, size);
+		}
+	}
+
+	private int indexOfLargerChild(SortData data, int parent, int size) {
+		int child = 2 * parent + 1;
+		if (child + 1 < size && data.less(child, child + 1)) {
+			++child;
+		}
+		return child;
 	}
 }
